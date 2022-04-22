84b6f1a4158bff89ac107e7f7e304a6bacdaab5a
David Valderrama Basante <1006171282@u.icesi.edu.co>
2022-04-22 10:58:30 -0500

    seguimiento12 hacer ultimos dos requerimientos

---
+++ src/main/Main.java
@@ -0,0 +1,44 @@
+package main;
+
+import java.util.Scanner;
+import model.*;
+
+public class Main {
+	
+	static Scanner sc = new Scanner(System.in);
+	static List list = new List();
+	public static void main(String[] args) {
+		List list = new List();
+		int option = 0;
+		do {
+			System.out.println("1. Asignar turno" + 
+					"\n2. Mostrar turno"+
+					"\n3. Pasar turno" + 
+					"\n4. Eliminar turno actual"+
+					"\n0. Cerrar programa");
+			option = sc.nextInt();
+			switchMenu1(option);
+		}while(option != 0);
+		
+		
+	
+	}
+	public static void switchMenu1(int option) {
+		switch(option) {
+		case 1: 
+			System.out.println("Entre el turno a asignar");
+			int turno = sc.nextInt();
+			list.addLast(new Node(turno));
+			list.print();
+			break;
+		case 2:
+			list.mostrarTurno();
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		}
+	}
+
+}
---
+++ src/model/List.java
@@ -0,0 +1,58 @@
+package model;
+
+public class List {
+	
+	private Node head;
+	private Node actual;
+	
+	public void addLast(Node n) {
+		if(head == null) {
+			head = n;
+			head.setNext(head);
+			head.setPrev(head);
+			return;
+		}else {
+			Node tail = head.getPrev();
+			head.setPrev(n);
+			tail.setNext(n);
+			
+			n.setNext(head);
+			n.setPrev(tail);
+			return;
+		}
+	}
+
+	
+	public void print() {
+		System.out.println("-------------");
+		print(head);
+		System.out.println("-------------");
+	}
+	private void print(Node n) {
+		if(n.getNext() == head) {
+			System.out.println(n.getValue());
+			return;
+		}
+		System.out.println( n.getValue());
+		print(n.getNext());
+	}
+	
+	public void mostrarTurno() {
+		mostrarTurno(actual);
+	}
+	private void mostrarTurno(Node actual) {
+		if(actual == null) {
+			System.out.println("no hay turnos aún");
+			return;
+		}else {
+			System.out.println(actual.getValue());
+			actual.setNext(actual.getNext());
+		}
+		
+	}
+	
+	
+	
+	
+
+}
